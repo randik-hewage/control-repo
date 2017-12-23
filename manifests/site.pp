010d0576e4bd37ed4e3a3a38bca42c8981696408
randikaudara <randikaudara@gmail.com>
2017-12-23 18:30:16 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,4 +29,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  # test code manager
+  notify{'Puppet cert test Control repo':}
 }
