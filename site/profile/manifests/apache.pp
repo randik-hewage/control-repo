c9dc26193d96d02779b97750611a94cd828010c5
Randika Hewage <randikaudara@gmail.com>
2017-12-27 00:22:37 +0530

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,4 +1,19 @@
-class pofile::apache () {
-  include apache
-  
+class profile::apache () {
+  class { '::apache':
+    default_vhost     => false
+    default_ssl_vhost => false,
+}
+
+$doc_root = "/var/www/${facts['fqdn']}"
+
+apache::vhost { $facts['fqdn']:
+  docroot => $doc_root,
+  port => 80,
+  before => File["${doc_root}/index.html"],
+}
+
+file { "${doc_root}/index.html"
+  ensure => file,
+  content => "This is a test page",
+  }  
 }
